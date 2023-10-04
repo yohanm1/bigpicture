0414c8816ba4998aa559bcf8b425f0b4c46eadfb
kmhan1221 <kmhan1221@gmail.com>
2023-10-04 00:03:54 -0700

    Add skeleton for topnav, home, trending, categories, settings components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { SearchTrendingComponent } from './search-trending/search-trending.component';
+import { SettingsComponent } from './settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'search-trending', component: SearchTrendingComponent },
+  { path: 'settings', component: SettingsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './navigation/top-nav/top-nav.component';
+import { HomeComponent } from './home/home.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { SearchTrendingComponent } from './search-trending/search-trending.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    HomeComponent,
+    CategoriesComponent,
+    SearchTrendingComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
