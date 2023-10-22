0d34c15509cee91f7ee8a4d06e54c74e1bdb9d62
Yohan Moon <yohanmoon01@gmail.com>
2023-10-22 14:28:03 -0500

    add a backend

---
+++ bigpicture-backend/src/main/java/com/bigpicture/bigpicturebackend/BigpictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bigpicture.bigpicturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigpictureBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigpictureBackendApplication.class, args);
+	}
+
+}
---
+++ bigpicture-backend/src/test/java/com/bigpicture/bigpicturebackend/BigpictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigpicture.bigpicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigpictureBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
